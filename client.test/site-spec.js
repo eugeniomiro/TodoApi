56d7510b23b35c12e7e8a75c4d125df2d396ed70
Eugenio Miró <eugenio.miro@gmail.com>
2021-02-20 16:18:32 -0300

    add test for site

---
+++ client.test/site-spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+//var site = require("../public/js/site.js");
+
+describe('site', function() {
+    it ('should have document ready method', function() {
+        
+    });
+});
