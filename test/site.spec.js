6ec617a535b64894d6a84b0378695da7cd1cd87c
Eugenio Miró <eugenio.miro@gmail.com>
2021-02-21 07:55:27 -0300

    add first set of tests for getCount function

--- test/site.spec.js
+++ test/site.spec.js
@@ -1,7 +1,55 @@
 'use strict';
 
-describe('site', function() {
-    it ('should', function() {
-        
+describe('calling getCount with 0 argument', function () {
+    var p;
+    var table;
+
+    before(function () {
+        p = $("<p id='counter'>");
+        table = $("<table id='todo-list'>");
+        $(document.body).append(p)
+            .append(table);
+        getCount(0);
+    });
+
+    it('should display \'No to-do\'', function () {
+        assert.equal(p.text(), 'No to-do');
+    });
+    it('should hide todo list', function () {
+        assert.equal(table.css('display'), 'none');
+    });
+
+    after(function () {
+        p.remove();
+        p = null;
+        table.remove();
+        table = null;
+    });
+});
+
+describe('calling getCount with 1 argument', function () {
+    var p;
+    var table;
+
+    before(function () {
+        p = $("<p id='counter'>");
+        table = $("<table id='todo-list'>");
+        $(document.body).append(p)
+            .append(table);
+        getCount(1);
+    });
+
+    it('should display \'1 to-do\'', function () {
+        assert.equal(p.text(), '1 to-do');
+    });
+    it('should show todo list', function () {
+        assert.equal(table.css('display'), 'table');
+    });
+
+    after(function () {
+        p.remove();
+        p = null;
+        table.remove();
+        table = null;
     });
 });
