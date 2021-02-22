fae98f5860cb65f44b9cbe2f7ef381550e436647
Eugenio Miró <eugenio.miro@gmail.com>
2021-02-22 19:23:10 -0300

    transform getCount tests into data driven tests

--- test/site.spec.js
+++ test/site.spec.js
@@ -1,109 +1,40 @@
 'use strict';
 
-describe('calling getCount with no argument', function () {
-    var p;
-    var table;
+describe('getCount()', function () {
+    const tests = [
+        { arg: null, expected: { todo: 'No to-do', display: 'none' } },
+        { arg: 0, expected: { todo: 'No to-do', display: 'none' } },
+        { arg: 1, expected: { todo: '1 to-do', display: 'table' } },
+        { arg: 2, expected: { todo: '2 to-dos', display: 'table' } },
+    ];
+    tests.forEach(({ arg, expected }) => {
+        describe(`calling getCount(${arg === null ? '' : arg})`, () => {
+            let p;
+            let table;
 
-    before(function () {
-        p = $("<p id='counter'>");
-        table = $("<table id='todo-list'>");
-        $(document.body).append(p)
-            .append(table);
-        getCount();
-    });
+            before(function () {
+                p = $("<p id='counter'>");
+                table = $("<table id='todo-list'>");
 
-    it('should display \'No to-do\'', function () {
-        assert.equal(p.text(), 'No to-do');
-    });
-    it('should show todo list', function () {
-        assert.equal(table.css('display'), 'none');
-    });
+                $(document.body)
+                    .append(p)
+                    .append(table);
+                getCount(arg);
+            });
 
-    after(function () {
-        p.remove();
-        p = null;
-        table.remove();
-        table = null;
-    });
-});
+            it(`should display '${expected.todo}'`, function () {
+                assert.equal(p.text(), expected.todo);
+            });
+            it(`should display todo list as ${expected.display}`, function () {
+                assert.equal(table.css('display'), expected.display);
+            });
 
-describe('calling getCount with argument 0', function () {
-    var p;
-    var table;
-
-    before(function () {
-        p = $("<p id='counter'>");
-        table = $("<table id='todo-list'>");
-        $(document.body).append(p)
-            .append(table);
-        getCount(0);
-    });
-
-    it('should display \'No to-do\'', function () {
-        assert.equal(p.text(), 'No to-do');
-    });
-    it('should hide todo list', function () {
-        assert.equal(table.css('display'), 'none');
-    });
-
-    after(function () {
-        p.remove();
-        p = null;
-        table.remove();
-        table = null;
-    });
-});
-
-describe('calling getCount with argument 1', function () {
-    var p;
-    var table;
-
-    before(function () {
-        p = $("<p id='counter'>");
-        table = $("<table id='todo-list'>");
-        $(document.body).append(p)
-            .append(table);
-        getCount(1);
-    });
-
-    it('should display \'1 to-do\'', function () {
-        assert.equal(p.text(), '1 to-do');
-    });
-    it('should show todo list', function () {
-        assert.equal(table.css('display'), 'table');
-    });
-
-    after(function () {
-        p.remove();
-        p = null;
-        table.remove();
-        table = null;
-    });
-});
-
-describe('calling getCount with argument 2', function () {
-    var p;
-    var table;
-
-    before(function () {
-        p = $("<p id='counter'>");
-        table = $("<table id='todo-list'>");
-        $(document.body).append(p)
-            .append(table);
-        getCount(2);
-    });
-
-    it('should display \'2 to-dos\'', function () {
-        assert.equal(p.text(), '2 to-dos');
-    });
-    it('should show todo list', function () {
-        assert.equal(table.css('display'), 'table');
-    });
-
-    after(function () {
-        p.remove();
-        p = null;
-        table.remove();
-        table = null;
+            after(function () {
+                p.remove();
+                p = null;
+                table.remove();
+                table = null;
+            });
+        });
     });
 });
