674a09d90a41f82e3c7ba5bdca1b25671221a39c
Eugenio Miró <eugenio.miro@gmail.com>
2021-02-24 05:54:35 -0300

    use gulp to build the entire app when launching from vscode

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,118 @@
-/// <binding Clean='clean' />
 "use strict";
 
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    server = require('karma').Server;
+const arg = require('./js/cmdLineArgs')(process.argv);
+const { clean, restore, build, test, publish, run } = require("gulp-dotnet-cli");
+const { task, series, src, dest } = require("gulp");
+const rimraf = require("rimraf");
+const concat = require("gulp-concat");
+const cssmin = require("gulp-cssmin");
+const uglify = require("gulp-uglify");
+const { server } = require('karma').Server;
 
-var webroot = "./public/";
-
-var paths = {
+let version = `1.0.` + (process.env.BUILD_NUMBER || '0');
+let configuration = arg.config || process.env.BUILD_CONFIGURATION || 'Release';
+let webroot = "./public/";
+let paths = {
     js: "./js/**/*.js",
     minJs: webroot + "js/**/*.min.js",
+    cmdLineArgs: "./js/cmdLineArgs.js",
     css: "./css/**/*.css",
     minCss: webroot + "css/**/*.min.css",
+    csProjs: '**/*.csproj',
+    csTestProjs: '**/*test*.csproj',
+    csWebProject: 'TodoApi.csproj',
     concatJsDest: webroot + "js/site.min.js",
     concatCssDest: webroot + "css/site.min.css",
 };
 
-gulp.task("clean:js", function(cb) {
+task("clean:dotnet", function() {
+    return src(paths.csProjs, {read: false})
+               .pipe(clean());
+});
+
+task("restore:dotnet", function() {
+    return src(paths.csProjs, {read: false})
+               .pipe(restore());
+});
+
+task("build:dotnet", series("restore:dotnet", function() {
+    return src(paths.csProjs, {read: false})
+               .pipe(build({
+                   configuration: configuration, 
+                   version: version
+                }));
+}));
+
+task("test:dotnet", series("build:dotnet", function() {
+    return src(paths.csTestProjs, {read: false})
+               .pipe(test());
+}));
+
+task("publish:dotnet", series("test:dotnet", function() {
+    return src(paths.csWebProject, {read: false})
+               .pipe(publish({
+                   configuration: configuration, 
+                   version: version
+                }));
+}));
+////convert a project to a nuget package
+//gulp.task('pack', series('build:dotnet', () => {
+//    return src('**/TestLibrary.csproj', {read: false})
+//                .pipe(pack({
+//                            output: path.join(process.cwd(), 'nupkgs') , 
+//                            version: version
+//                            }));
+//}));
+////push nuget packages to a server
+//gulp.task('push', series('pack', () => {
+//    return src('nupkgs/*.nupkg', {read: false})
+//                .pipe(push({
+//                    apiKey: process.env.NUGET_API_KEY, 
+//                    source: 'https://myget.org/f/myfeedurl'}));
+//}));
+
+task("clean:js", function(cb) {
     rimraf(paths.concatJsDest, cb);
 });
 
-gulp.task("clean:css", function(cb) {
+task("clean:css", function(cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", gulp.series("clean:js", "clean:css"));
+task("clean", series("clean:js", "clean:css", "clean:dotnet"));
 
-gulp.task("min:js", function() {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+task("min:js", function() {
+    return src([paths.js, "!" + paths.minJs, "!" + paths.cmdLineArgs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
-        .pipe(gulp.dest("."));
+        .pipe(dest("."));
 });
 
-gulp.task("min:css", function() {
-    return gulp.src([paths.css, "!" + paths.minCss])
+task("min:css", function() {
+    return src([paths.css, "!" + paths.minCss])
         .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
-        .pipe(gulp.dest("."));
+        .pipe(dest("."));
 });
 
-gulp.task("min", gulp.series("min:js", "min:css"));
-gulp.task("clean-min", gulp.series("clean", "min"));
-gulp.task('test', function(done) {
+task("min", series("min:js", "min:css"));
+task('test:js', function(done) {
     new server({
         configFile: __dirname + "/karma.conf.js",
         singleRun: true,
     }, done).start();
 });
 
-gulp.task('tdd', function(done) {
+task("test", series("test:js", "test:dotnet"))
+
+task('run:dotnet', series("test", ()=>{
+    return src(paths.csWebProject, {read: false})
+                .pipe(run());
+}));
+
+task("publish", series("clean", "min", "publish:dotnet"));
+
+task('tdd', function(done) {
     new server({
         configFile: __dirname + "/karma.conf.js",
         autoWatch: true,
---
+++ js/cmdLineArgs.js
@@ -0,0 +1,21 @@
+// see: https://www.sitepoint.com/pass-parameters-gulp-tasks/
+
+module.exports = argList => {
+    let arg = {}, a, opt, thisOpt, curOpt;
+    for (a = 0; a < argList.length; a++) {
+      thisOpt = argList[a].trim();
+      opt = thisOpt.replace(/^\-+/, '');
+  
+      if (opt === thisOpt) {  
+        // argument value
+        if (curOpt) arg[curOpt] = opt;
+        curOpt = null;  
+      } else {  
+        // argument name
+        curOpt = opt;
+        arg[curOpt] = true;  
+      }  
+    }
+  
+    return arg;
+};
