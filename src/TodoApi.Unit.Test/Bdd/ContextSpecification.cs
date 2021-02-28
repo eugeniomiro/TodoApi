8183c4a7afee951579e9ee747e2a9c3cf812e81f
Eugenio Miró <eugenio.miro@gmail.com>
2021-02-28 19:49:12 -0300

    added first test

---
+++ src/TodoApi.Unit.Test/Bdd/ContextSpecification.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// see: https://saintgimp.org/2009/01/20/bdd-with-mstest/
+namespace TodoApi.Unit.Test.Bdd
+{
+    public class ContextSpecification
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Context();
+            BecauseOf();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            CleanUp();
+        }
+
+        protected virtual void Context() { }
+        protected virtual void BecauseOf() { }
+        protected virtual void CleanUp() { }
+    }
+}
--- src/TodoApi.Unit.Test/Controllers/TodoControllerTests.cs
+++ src/TodoApi.Unit.Test/Controllers/TodoControllerTests.cs
@@ -2,12 +2,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TodoApi.Unit.Test.Controllers
 {
-    [TestClass]
-    public class TodoControllerTests
+    using Bdd;
+    using Models;
+    using TodoApi.Controllers;
+
+    public static class TodoControllerTests 
     {
-        [TestMethod]
-        public void TestMethod1()
+        public class TodoControllerContext : ContextSpecification
         {
+            protected TodoController _sut;
+            
+            protected override void Context()
+            {
+                _sut = new TodoController(default(TodoContext));
+            }
+
+            [TestClass]
+            public class When_Constructor_Is_Called_With_Null_Parameter : TodoControllerContext
+            {
+                [TestMethod]
+                public void Calling_GetAll_Method__Throws_Exception()
+                {
+                    try
+                    {
+                        _sut.GetAll().Wait();
+                    }
+                    catch (System.AggregateException)
+                    {                        
+                        return;
+                    }
+                    Assert.Fail();
+                }
+            }
         }
     }
 }
