7765ed36d967c3559a5db91356e65d461b0e9ae7
Eugenio Miró <eugenio.miro@gmail.com>
2021-03-04 18:44:08 -0300

    add TodoService

---
+++ TodoApi.Service.Unit.Test/TodoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleSpec.Bdd;
+
+namespace TodoApi.Service.Unit.Test
+{
+    using DataAccess;
+    using DataAccess.Concrete;
+
+    public static class TodoServiceTests
+    {
+        [TestClass]
+        public class TodoServiceContext: ContextSpecification
+        {
+            [TestMethod]
+            public void Can_Create_TodoService()
+            {
+                new TodoService(new TodoRepository(new TodoContext(new DbContextOptionsBuilder<TodoContext>()
+                                                                        .UseInMemoryDatabase("TodoService tests")
+                                                                        .Options)));
+            }
+        }
+    }
+}
---
+++ src/TodoApi.Service/TodoService.cs
@@ -0,0 +1,14 @@
+﻿using TodoApi.DataAccess.Contract;
+
+namespace TodoApi.Service
+{
+    public class TodoService
+    {
+        public TodoService(ITodoRepository todoRepository)
+        {
+            this.todoRepository = todoRepository;
+        }
+
+        private readonly ITodoRepository todoRepository;
+    }
+}
