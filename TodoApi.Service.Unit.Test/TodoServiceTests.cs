6d32f2377588a2ce24a451ee98cd3ff0e3cafa5e
Eugenio Miró <eugenio.miro@gmail.com>
2021-03-05 07:26:28 -0300

    * Move TodoService into Concrete namespace * Create GetAllAsync method through TDD

--- TodoApi.Service.Unit.Test/TodoServiceTests.cs
+++ TodoApi.Service.Unit.Test/TodoServiceTests.cs
@@ -1,23 +1,61 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using SimpleSpec.Bdd;
 
 namespace TodoApi.Service.Unit.Test
 {
-    using DataAccess;
-    using DataAccess.Concrete;
+    using DataAccess.Contract;
+    using Domain.Models;
+    using Service.Concrete;
 
     public static class TodoServiceTests
     {
         [TestClass]
         public class TodoServiceContext: ContextSpecification
         {
-            [TestMethod]
-            public void Can_Create_TodoService()
+            [TestClass]
+            public class TodoService_Object
             {
-                new TodoService(new TodoRepository(new TodoContext(new DbContextOptionsBuilder<TodoContext>()
-                                                                        .UseInMemoryDatabase("TodoService tests")
-                                                                        .Options)));
+                [TestMethod]
+                public void Can_Be_Created()
+                {
+                    new TodoService(new Mock<ITodoRepository>().Object);
+                }
+            }
+
+            [TestClass]
+            public class When_Calling_GetAll_Method_From_Sut: TodoServiceContext
+            {
+                protected override void Context()
+                {
+                    // Arrange
+                    repositoryMock = new Mock<ITodoRepository>();
+                    repositoryMock.Setup(r => r.GetAllAsync())
+                                  .ReturnsAsync(new List<TodoItem> { new TodoItem { } });
+                    var sut = new TodoService(repositoryMock.Object);
+
+                    // Act
+                    list = sut.GetAllAsync().Result;
+                }
+
+                [TestMethod]
+                public void It_Calls_GetAll_Repository_Method()
+                {
+                    // Assert
+                    repositoryMock.Verify(r => r.GetAllAsync(), Times.Once);
+                }
+
+                [TestMethod]
+                public void Resulting_List_Has_Data()
+                {
+                    // Assert
+                    Assert.AreEqual(1, list.Count());
+                }
+
+                private IEnumerable<TodoItem> list;
+                private Mock<ITodoRepository> repositoryMock;
             }
         }
     }
---
+++ src/TodoApi.Service/Concrete/TodoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoApi.DataAccess.Contract;
+using TodoApi.Domain.Models;
+
+namespace TodoApi.Service.Concrete
+{
+    public class TodoService
+    {
+        public TodoService(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        private readonly ITodoRepository _todoRepository;
+
+        public async Task<IEnumerable<TodoItem>> GetAllAsync()
+        {
+            return await _todoRepository.GetAllAsync();
+        }
+    }
+}
