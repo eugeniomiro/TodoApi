7575726ec4a6c18a7a9c1d49bb4feac3d410f2fd
Eugenio Miró <eugenio.miro@gmail.com>
2021-03-08 01:47:29 -0300

    Extract ITodoService interface

---
+++ src/TodoApi.Service/Contract/ITodoService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoApi.Domain.Models;
+
+namespace TodoApi.Service.Contract
+{
+    public interface ITodoService
+    {
+        Task<TodoItem> CreateAsync(TodoItem todoItem);
+        Task<TodoItem> DeleteAsync(long id);
+        Task<IEnumerable<TodoItem>> GetAllAsync();
+        Task<TodoItem> GetTodoAsync(long id);
+        Task<TodoItem> UpdateAsync(long id, TodoItem todoItem);
+    }
+}
